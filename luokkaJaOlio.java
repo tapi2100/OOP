463c9dcbeaa6888b706a7be969384f1a6bf0f1f5
tapi2100 <90243328+tapi2100@users.noreply.github.com>
2022-01-12 19:00:54 +0200

    Add files via upload

---
+++ luokkaJaOlio.java
@@ -0,0 +1,17 @@
+
+public class luokkaJaOlio {
+
+	public static void main(String[] args) {
+
+	}
+
+}
+
+class Kiekko {
+	protected String plastic;
+	protected int weight;
+	protected int speed;
+	protected int glide;
+	protected int turn;
+	protected int fade;
+}
